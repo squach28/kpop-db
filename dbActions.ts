ce7cb2a5e9a7027ae2030c2292f5c779b4ffe577
squach28 <squach3443@gmail.com>
2024-05-09 10:56:26 -0700

    added utils for downloadImage, createDirectory, and deleteDirectory

--- dbActions.ts
+++ dbActions.ts
@@ -1,6 +1,8 @@
 import { pool } from "./db";
+import { bucket } from "./firebase";
 
-const insertIdol = async (idol: Idol) => {
+// inserts image into pg db
+export const insertIdol = async (idol: Idol) => {
   try {
     const INSERT_QUERY =
       "INSERT INTO idols (name, birth_name, dob, image_url, nationality, group_id) values ($1, $2, $3, $4, $5, $6) ON CONFLICT DO NOTHING RETURNINiG ID";
@@ -29,11 +31,24 @@ const insertIdol = async (idol: Idol) => {
   }
 };
 
-const insertImage = (groupName: string, idolName: string, fileType: string) => {
+// inserts an image (file) into firebase storage
+// returns uploaded file if successful
+// returns null if file fails to upload
+export const insertImage = async (
+  groupName: string,
+  idolName: string,
+  fileType: string,
+) => {
   try {
     const idolNameWithUnderScores = idolName.replace(/\s/, "_");
     const fileName = `./${groupName}/${idolName}.${fileType}`;
     const destination = `/${groupName}/${idolNameWithUnderScores}.${fileType}`;
+    const upload = await bucket.upload(fileName, {
+      destination,
+      gzip: true,
+    });
+
+    return upload[1];
   } catch (e) {
     console.log(e);
     return null;
---
+++ utils.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import fs from "fs";
+
+// downloads an image from a name (idol), groupName, and url to download the image from
+export const downloadImage = async (
+  name: string,
+  groupName: string,
+  url: string,
+) => {
+  try {
+    const idolName = name.toLowerCase();
+    const res = await axios({
+      url,
+      responseType: "stream",
+    });
+
+    return new Promise((resolve, reject) => {
+      const contentType = res.headers["content-type"];
+      const splittedType = contentType.split("/");
+      const fileType = splittedType[splittedType.length - 1];
+
+      res.data
+        .pipe(fs.createWriteStream(`./${groupName}/${idolName}.${fileType}`))
+        .on("finish", () => resolve({ groupName, idolName, fileType }))
+        .on("error", (e) => reject(e));
+    });
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
+
+// creates a directory for a kpop group
+export const createGroupDirectory = (groupName: string) => {
+  if (!fs.existsSync(groupName)) {
+    return;
+  }
+  fs.mkdirSync(groupName);
+};
+
+// removes all files from a directory and deletes the directory
+export const deleteGroupDirectory = (groupName) => {
+  if (fs.existsSync(groupName)) {
+    fs.rmSync(`./${groupName}`, { recursive: true, force: true });
+  }
+};
